36bb806129c569c414e969619736cff4670c7825
niedong <niedong0816@126.com>
2021-12-31 11:05:56 +0800

    Update template.html

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name='greport',
-    version='0.1.9',
+    version='0.1.10',
     description='Generate html from googletest xml file',
     long_description=long_description,
     long_description_content_type='text/markdown',
